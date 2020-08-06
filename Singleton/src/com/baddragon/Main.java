97058e47f5bd889bc77e08e5a2ef0cc05c39e596
BloodyRainRage <bloodyrainrage@gmail.com>
2020-08-06 03:32:20 +0300

    Singleton

---
+++ Singleton/src/com/baddragon/Main.java
@@ -0,0 +1,10 @@
+package com.baddragon;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //there is nothing to show
+        //check Singleton implementation
+        //that's all
+    }
+}
