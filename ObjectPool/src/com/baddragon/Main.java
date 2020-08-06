6d2f817fa67f4c08635383e45eb1be1930762eb3
BloodyRainRage <bloodyrainrage@gmail.com>
2020-08-06 04:26:50 +0300

    ObjectPool
    ObjectPool missprint fix

--- ObjectPool/src/com/baddragon/Main.java
+++ ObjectPool/src/com/baddragon/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //we have a pool of objects which has a concrete amount of items
+        //we have a pool of objects which has a strict amount of items
         //for example: if we want to hire a car on parking, we are checking all cars
         //until we find non-hired car, then we reserve it for ourselves
         //that's the principe of Object Pool
